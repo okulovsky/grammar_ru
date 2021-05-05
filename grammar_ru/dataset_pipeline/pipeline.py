51825bc4ddf28d847d3d7adbf4e67b7353c67a20
HolyPrapor <tolya12345w@gmail.com>
2021-05-05 15:25:18 +0500

    Made pipeline

--- grammar_ru/dataset_pipeline/pipeline.py
+++ grammar_ru/dataset_pipeline/pipeline.py
@@ -1,4 +1,13 @@
+from typing import *
+from grammar_ru.common.separator import Separator
+from .nlp_analyzer import NlpAnalyzer
+import pandas as pd
 
 
-def pipeline():
-    pass
+def run_pipeline(analyzers: List[NlpAnalyzer], text: List[str]) -> pd.DataFrame:
+    result_df = Separator.separate_paragraphs(text)
+
+    for analyzer in analyzers:
+        result_df = analyzer.apply(result_df)
+
+    return result_df
