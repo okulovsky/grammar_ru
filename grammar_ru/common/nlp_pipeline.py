e1d11ebd52ebed2f084e0d36bf46ce8d4e0e75b1
Ilya Tzoop <tolya12345w@gmail.com>
2021-05-08 20:16:19 +0500

    + NlpPipeline

--- grammar_ru/common/nlp_pipeline.py
+++ grammar_ru/common/nlp_pipeline.py
@@ -3,10 +3,10 @@
 # 2) NlpPreprocessingPipeline
 
 # Scenarios:
-# 1) I should be able to send NlpAnalizationPipeline to create dataset to cloud
+# 1) I should be able to send NlpAnalizationPipeline to create dataset on cloud
 #   because it can require a lot of resources. It means, that NlpAnalyzationPipeline should
 #   be either inherited from FeaturizationJob or be able to return it conviniently.
-# 2) I should be able to apply NlpPreprocessingPipeline to apply preprocessing before passing
+# 2) I should be able to use NlpPreprocessingPipeline to apply preprocessing before passing
 #   the data to the algorithm.
 
 
@@ -17,3 +17,21 @@
 # contained some methods for model training, but I think that this architecture would not be
 # flexible and convenient enough for testing and experimenting with data.
 # So the user should be careful when analyzing and applying its results.
+
+from .nlp_analyzation_pipeline import NlpAnalyzationPipeline
+from tg.common.ml import dft
+from typing import *
+import pandas as pd
+
+
+class NlpPipeline:
+    def __init__(self, analyzation_pipeline: NlpAnalyzationPipeline, preprocessing_pipeline: dft.DataFrameTransformer):
+        self._analyzation_pipeline = analyzation_pipeline
+        self._preprocessing_pipeline = preprocessing_pipeline
+
+    def run_on_text(self, text: List[str]) -> pd.DataFrame:
+        analyzed_text = self._analyzation_pipeline.analyze_text(text)
+        return self._preprocessing_pipeline.transform(analyzed_text)  # Data is not fitted. How to handle this?
+
+    def run_on_string(self, string: str) -> pd.DataFrame:
+        return self.run_on_text([string])
--- grammar_ru/common/nlp_preprocessing_pipeline.py
+++ grammar_ru/common/nlp_preprocessing_pipeline.py
@@ -1,3 +1,12 @@
+from grammar_ru.common.nlp_preprocessor import NlpPreprocessor
+from tg.common.ml import dft
+from typing import *
+
+# Can be completely replaced with dft.DataFrameTransformer?
+
+
 class NlpPreprocessingPipeline:
-    def __init__(self):
-        pass
+    def __init__(self, preprocessors: List[NlpPreprocessor]):
+        self._preprocessors = preprocessors
+
+    pass
