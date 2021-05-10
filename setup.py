094be61446ec30a68ef11438262bdb3b714831e8
HolyPrapor <tolya12345w@gmail.com>
2021-05-10 15:07:06 +0500

    Check deps

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(name='grammar_ru',
       description='Demo',
       packages=find_packages(),
       install_requires=[
-        'training_grounds',
-        'slovnet==0.5.0',
-        'pyenchant==3.2.0',
-        'pymorphy2==0.9.1'
+          'training_grounds',
+          'slovnet==0.5.0',
+          'pyenchant==3.2.0',
+          'pymorphy2==0.9.1'
       ],
-      include_package_data = True,
+      include_package_data=True,
       zip_safe=False
-)
+      )
