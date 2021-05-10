5b1e6db3cf63b0147651497f8e799343629d9328
HolyPrapor <tolya12345w@gmail.com>
2021-05-10 16:40:18 +0500

    Some natasha helper functions

--- grammar_ru/common/natasha.py
+++ grammar_ru/common/natasha.py
@@ -1,12 +1,43 @@
 from typing import *
 import pandas as pd
-from .architecture.validations import validations
+from .architecture import validations
+from .architecture import Separator
+from nerus import NerusDoc, load_nerus
+from itertools import reduce
 
 
-def make_dataframe_from_nerus() -> pd.DataFrame:
-    pass
+def get_text_from_nerus_doc(doc: NerusDoc) -> str:
+    return reduce(lambda x, y: x + " " + y, map(lambda x: x.text, doc.sents))
+
+
+def make_dataframe_from_nerus(doc_amount: int, skip: int = 0) -> pd.DataFrame:
+    docs = load_nerus('nerus_lenta.conllu.gz')
+
+    paragraphs = []
+
+    for i in range(doc_amount + skip):
+        doc = next(docs, None)
+
+        if i < skip:
+            continue
+
+        # No docs are left
+        if not doc:
+            break
+
+        paragraphs.append(get_text_from_nerus_doc(doc))
+
+    return Separator.separate_paragraphs(paragraphs)
 
 
 def create_chunks_from_dataframe(df: pd.DataFrame) -> List[List[str]]:
     validations.ensure_df_contains(validations.WordCoordinates)
-    pass
+    chunks = []
+
+    for row in df:
+        if row['sentence_id'] - 1 == len(chunks):
+            chunks.append([])
+
+        chunks[row['sentence_id'] - 1].append(row['word'])
+
+    return chunks
