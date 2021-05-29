8989f968dec408375616f82b6c963270c4425354
Yuri Okulovsky <yuri.okulovsky@gmail.com>
2021-05-29 13:36:48 +0200

    + service info in rep. algorithm
    + service info in rep algorithm
    
    
    WIP

--- tests/test_algorithms/test_repetitions.py
+++ tests/test_algorithms/test_repetitions.py
@@ -14,8 +14,7 @@ class RepetitionsAlgorithmTestCase(TestCase):
 
     def test_normal(self):
         df = RepetitionsAlgorithm(50, False, True, False).run_on_string(text)
-        print(df)
-        self.assertListEqual([2, 7, 10], list(df.loc[~df.repetition_status].word_id))
+        self.assertListEqual([2,7, 10], list(df.loc[~df.repetition_status].word_id))
 
     def test_tikhonov(self):
         df = RepetitionsAlgorithm(50, False, False, True).run_on_string(text)
@@ -31,4 +30,13 @@ class RepetitionsAlgorithmTestCase(TestCase):
 
     def test_on_empty(self):
         df = RepetitionsAlgorithm(50, True, True, True).run_on_string("Здесь нет ошибок", [])
-        print(df)
+        self.assertListEqual([True,True,True], list(df.repetition_status))
+
+    def test_extended_mode(self):
+        df = RepetitionsAlgorithm(50,True,True,True,True).run_on_string('Един един единый двуединый')
+        self.assertListEqual([-1,0,1,2], list(df.repetition_reference))
+        self.assertListEqual([None,'simple','normal','tikhonov'], list(df.repetition_algorithm))
+
+    def test_tikhonov_join(self):
+        df = RepetitionsAlgorithm(50,True,True,True,True).run_on_string('двуединое единообразие')
+        self.assertListEqual([True,False], list(df.repetition_status))
