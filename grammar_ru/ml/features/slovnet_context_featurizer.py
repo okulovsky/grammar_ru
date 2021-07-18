ebe39f49f4fda3f28be8665ce82207aab8a4dc2a
Ilya Tzoop <tolya12345w@gmail.com>
2021-07-18 20:26:58 +0500

    Added note

--- grammar_ru/ml/features/slovnet_context_featurizer.py
+++ grammar_ru/ml/features/slovnet_context_featurizer.py
@@ -4,6 +4,11 @@ from yo_fluq_ds import *
 
 
 class SlovnetContextFeaturizer(Featurizer):
+    _wordId = "word_id"
+    _parentId = "syntax_parent_id"
+    _shift = "shift"
+    _relativeWordId = "relative_word_id"
+
     def __init__(self, max_shift=3):
         self.max_shift = max_shift
 
@@ -12,34 +17,40 @@ class SlovnetContextFeaturizer(Featurizer):
 
     def featurize(self, df):
         prepared_df = df
-        if "syntax_parent_id" not in df.columns:
+        if self._parentId not in df.columns:
             prepared_df = SlovnetFeaturizer().featurize(df)
 
         return self._extract_context(prepared_df)
 
+    """
+        Non-optimized (iterative approach is used)
+        Can be optimized by some vectorization operations.
+        (However, we are unable to get rid of iterative approach everywhere in this function)
+    """
     def _extract_context(self, df: pd.DataFrame):
-        parent_df = df.reset_index().sort_values(by=["syntax_parent_id", "word_id"]).set_index([
-            "syntax_parent_id", "word_id"])
+        parent_df = df.reset_index().sort_values(by=[self._parentId, self._wordId]).set_index([
+            self._parentId, self._wordId])
 
         new_rows = []
 
         for word_id in df.index:
-            parent_id = df.loc[word_id, "syntax_parent_id"]
+            parent_id = df.loc[word_id, self._parentId]
             shift = 0
             relative_id = word_id
 
-            # Seeking for parent -> grandparent -> ...
+            # Looking for parent -> grandparent -> ...
             for _ in range(self.max_shift):
                 shift += 1
-                relative_id = df.loc[relative_id, "syntax_parent_id"]
+                relative_id = df.loc[relative_id, self._parentId]
                 if relative_id == -1:
                     break
-                new_rows.append({'word_id': word_id, 'shift': shift, 'relative_word_id': relative_id})
+                new_rows.append({self._wordId: word_id, self._shift: shift, self._relativeWordId: relative_id})
 
-            # Seeking for brothers, sisters ...
+            # Looking for brothers, sisters ...
             for idx in parent_df.loc[parent_id].index if parent_id in parent_df.index else []:
-                new_rows.append({'word_id': word_id, 'shift': 0, 'relative_word_id': idx})
+                new_rows.append({self._wordId: word_id, self._shift: 0, self._relativeWordId: idx})
 
+            # Looking for children...
             ids = [word_id]
             for i in range(1, self.max_shift + 1):
                 child_ids = []
@@ -49,8 +60,8 @@ class SlovnetContextFeaturizer(Featurizer):
                         child_ids.append(idx)
 
                 for child in child_ids:
-                    new_rows.append({'word_id': word_id, 'shift': -i, 'relative_word_id': child})
+                    new_rows.append({self._wordId: word_id, self._shift: -i, self._relativeWordId: child})
 
                 ids = child_ids
 
-        return pd.DataFrame(new_rows).set_index("word_id")
+        return pd.DataFrame(new_rows).set_index(self._wordId)
