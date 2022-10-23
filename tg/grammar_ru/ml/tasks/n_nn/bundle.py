0e3bf252e9e55ae8c3c151aec59b25bc505e38d4
invis166 <kolesnikovv028@gmail.com>
2022-10-23 19:16:40 +0500

    change word extracting method

--- tg/grammar_ru/ml/tasks/n_nn/bundle.py
+++ tg/grammar_ru/ml/tasks/n_nn/bundle.py
@@ -3,24 +3,17 @@ import typing as tp
 import pandas as pd
 
 from .word_normalizer import WordNormalizer, NltkWordStemmer
-
-
-_single_n_regex = r'[^н]н[^н](?!.*?нн)'  # matches only 'н' not followed by 'нн'
-_double_n_regex = r'нн(?!.+?н)'  # matches only 'нн' not followed by 'н'
+from .regular_expressions import single_n_regex, double_n_regex
 
 
 def _extract_words(dfs: tp.Iterable[pd.DataFrame]) -> tp.Tuple[tp.Set[str], tp.Set[str]]:
     words_with_single_n = set()
     words_with_double_n = set()
     for df in dfs:
-        words_with_single_n.update(df[df['word'].str.contains(_single_n_regex)]['word'])
-        words_with_double_n.update(df[df['word'].str.contains(_double_n_regex)]['word'])
+        words_with_single_n.update(df[df['word'].str.contains(single_n_regex)]['word'])
+        words_with_double_n.update(df[df['word'].str.contains(double_n_regex)]['word'])
 
     return words_with_single_n, words_with_double_n
-
-
-def transform_word_with_double_to_word_with_single(word: str) -> str:
-    return word[::-1].replace('нн', 'н', 1)[::-1]
 
 
 def build_dictionary(
@@ -29,11 +22,9 @@ def build_dictionary(
         ) -> tp.Set[str]:
     words_with_single_n, words_with_double_n = _extract_words(dfs)
 
-    words_with_double_replaced_to_single = set(map(transform_word_with_double_to_word_with_single, words_with_double_n))
-
     intersection = (
             set(map(normalizer.normalize_word, words_with_single_n))
-            & set(map(normalizer.normalize_word, words_with_double_replaced_to_single))
+            & set(map(normalizer.normalize_word, words_with_double_n))
     )
 
     return intersection
