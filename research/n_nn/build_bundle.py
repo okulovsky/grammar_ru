b526a84c0a76a14376d3057184f090fd5460bf95
invis166 <kolesnikovv028@gmail.com>
2022-10-28 13:06:48 +0500

    bundle building

---
+++ research/n_nn/build_bundle.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import yo_fluq_ds as yfds
+
+from research.common import build_bundle
+from tg.grammar_ru.common import Loc
+from tg.grammar_ru.ml.tasks.train_index_builder.index_builders import NNnIndexBuilder
+from tg.grammar_ru.ml.tasks.n_nn.word_normalizer import RegexNormalizer
+from tg.grammar_ru.ml.tasks.n_nn.bundle import build_dictionary
+
+
+VOCAB_FILE = Path(__file__).parent/'words.json'
+
+
+if not (VOCAB_FILE).exists():
+    print('Collecting words')
+    words = list(filter(lambda x: len(x) > 3, build_dictionary(
+        build_bundle.read_data(build_bundle.LENTA_CORPUS_PATH),
+        RegexNormalizer()))
+        )
+    yfds.FileIO.write_json(words, VOCAB_FILE)
+
+
+index_path = Loc.bundles_path/'n_nn/prepare/raw/raw_partial.zip'
+if not (Path(__file__)/index_path).exists():
+    print('Building index')
+    words = yfds.FileIO.read_json(VOCAB_FILE)
+    index_builder = NNnIndexBuilder(words, word_normalizer=RegexNormalizer())
+    build_bundle.build_index(index_builder, build_bundle.LENTA_CORPUS_PATH, index_path, 1_000_000)
+
+
+features_path = Loc.bundles_path/'n_nn/prepare/feat/feat_partial.zip'
+if not (Path(__file__)/features_path).exists():
+    print('Extracting features')
+    build_bundle.featurize_index(index_path, features_path, workers=3)
+
+
+bundle_name = 'partial'
+bundle_path = Loc.bundles_path/f'n_nn/{bundle_name}'
+if not (Path(__file__)/bundle_path).exists():
+    print('Building bundle')
+    build_bundle.assemble(5, features_path, bundle_path)
