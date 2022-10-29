f8c6b7da666e1d2c464baed3c7d965accd3591eb
Yuri Okulovsky <yuri.okulovsky@gmail.com>
2022-10-29 15:13:26 +0200

    Updated contextual_binding
    Now it's a parameter to make it bidirectional
    You can now use it standalone, so it's compatible with easier_torch approach

--- tg/grammar_ru/ml/components/contextual_binding.py
+++ tg/grammar_ru/ml/components/contextual_binding.py
@@ -22,6 +22,13 @@ class ContextualNetworkType(Enum):
     BidirectLSTM = 4
 
 
+class ContextPresentationType(Enum):
+    Simple = 0
+    Bidirectional = 1
+    BidirectionalMirrored = 2
+
+
+
 class ContextualBinding(ExtractorNetworkBinding):
     def __init__(self,
                  name: str,
@@ -40,8 +47,14 @@ class ContextualBinding(ExtractorNetworkBinding):
         self.context_builder = deepcopy(context_builder)
         self.extractor = deepcopy(extractor)
         self.debug = debug
+        self.context_presentation = ContextPresentationType.Simple
+
 
     def create_network_factory(self, task, input):
+        return self.assemble_network_factory()
+
+
+    def assemble_network_factory(self):
         if self.network_type == ContextualNetworkType.Plain:
             return btt.FullyConnectedNetwork.Factory(self.hidden_size).prepend_extraction(self.name)
         elif self.network_type == ContextualNetworkType.LSTM:
@@ -56,13 +69,20 @@ class ContextualBinding(ExtractorNetworkBinding):
             raise ValueError(f"Network type {self.network_type} is not recognized")
 
     def _create_extractor_internal(self, task, bundle):
+        return self.assemble_extractor()
+
+    def assemble_extractor(self):
         if self.network_type == ContextualNetworkType.Plain:
             eaf = btc.SimpleExtractorToAggregatorFactory(self.extractor, btc.PivotAggregator(True))
             fin = btc.PandasAggregationFinalizer()
         else:
             eaf = btc.SimpleExtractorToAggregatorFactory(self.extractor)
-            # fin = btt.LSTMFinalizer(self.reverse_order_in_lstm)
-            fin = LSTM_BicontextualFinalizer(mirror_concat = False)
+            if self.context_presentation == ContextPresentationType.Simple:
+                fin = btt.LSTMFinalizer(self.reverse_order_in_lstm)
+            elif self.context_presentation == ContextPresentationType.Bidirectional:
+                fin = LSTM_BicontextualFinalizer(mirror_concat = False)
+            else:
+                fin = LSTM_BicontextualFinalizer(mirror_concat=True)
 
         return btc.ContextExtractor(
             self.name,
@@ -72,3 +92,7 @@ class ContextualBinding(ExtractorNetworkBinding):
             fin,
             self.debug
         )
+
+
+class WordContextAssemblyPoint(ContextualBinding):
+    pass
