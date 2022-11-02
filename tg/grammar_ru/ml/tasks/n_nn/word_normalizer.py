d6db38616bac694686b2aafef6725617a84b06ee
invis166 <kolesnikovv028@gmail.com>
2022-11-02 16:59:26 +0500

    trying to understand the causes of artifacts in the collected words

--- tg/grammar_ru/ml/tasks/n_nn/word_normalizer.py
+++ tg/grammar_ru/ml/tasks/n_nn/word_normalizer.py
@@ -1,3 +1,5 @@
+import typing as tp
+from collections import defaultdict
 import re
 import abc
 
@@ -23,11 +25,22 @@ class NltkWordStemmer(WordNormalizer):
 class RegexNormalizer(WordNormalizer):
     def normalize_word(self, word: str) -> str:
         if (match := re.search(single_n_regex, word)):
-            return word[:match.start() + 1]
+            return word[:match.start() + 1].lower()
         elif (match := re.search(double_n_regex, word)):
-            return word[:match.start()]
+            return word[:match.start()].lower()
 
-        return word
+        return word.lower()
+
+
+class LoggingRegexNormalizer(RegexNormalizer):
+    def __init__(self) -> None:
+        self._map: tp.MutableMapping[str, tp.List[str]] = defaultdict(list)
+
+    def normalize_word(self, word: str) -> str:
+        normalized = super().normalize_word(word)
+        self._map[word].append(normalized)
+
+        return normalized
 
 
 class EmptyNormalizer(WordNormalizer):
