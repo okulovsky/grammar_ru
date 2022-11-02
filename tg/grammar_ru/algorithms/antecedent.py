2fa50d838980dbcf92884e6fe3ed24defbc48858
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2022-11-02 22:19:16 +0500

    Not sure iа antecedents finder is ready

--- tg/grammar_ru/algorithms/antecedent.py
+++ tg/grammar_ru/algorithms/antecedent.py
@@ -1,12 +1,54 @@
-from yo_fluq_ds import *
 from .architecture import NlpAlgorithm
-from ..ml.features import PyMorphyFeaturizer, SlovnetFeaturizer
+from ..ml.features import \
+    PyMorphyFeaturizer, \
+    SlovnetFeaturizer, \
+    NavecFeaturizer, \
+    SimpleFeaturizer
+import pandas as pd
 from ...common import DataBundle
 
 
 class AntecedentCandidatesAlgorithm(NlpAlgorithm):
-    def __init__(self, max_candidates: int = 10):
+    def __init__(self, dbs_query, max_candidates: int = 10):
+        self.pmf = PyMorphyFeaturizer()
+        self.slvnt = SlovnetFeaturizer()
+        self.navec = NavecFeaturizer()
         self.max_candidates = max_candidates
+        self.pronoun_replacer = self._get_proper_replace_dict(dbs_query)
+
+    def _get_proper_replace_dict(self, dbs_query):
+        proper_counts = pd.DataFrame()
+        for db in dbs_query:
+            db_counts = self._count_propers(db)
+            proper_counts = \
+                (pd.concat([proper_counts, db_counts])
+                   .groupby(['normal_form', 'gender'])
+                   .sum()
+                   .reset_index())
+        converter = {'femn': 'женщина',
+                     'masc': 'мужчина',
+                     'neut': 'существо',
+                     None: 'существо'}
+        return dict(zip(proper_counts.normal_form,
+                        proper_counts.gender.apply(lambda x: converter[x])))
+
+    @staticmethod
+    def _is_proper(noun: str, db: DataBundle):
+        indices = list(db.pymorphy[db.pymorphy['normal_form'] == noun].index)
+        words = db.src[db.src.index.isin(indices)]['word']
+        return words.str[0].str.isupper().all() and words.count() > 2
+
+    def _count_propers(self, db: DataBundle):
+        self.pmf.featurize(db)
+        nouns = db.pymorphy[db.pymorphy['POS'] == 'NOUN']
+        counted = nouns.groupby(['normal_form'])['normal_form'].count()
+        propers = counted[counted.index.map(lambda x: self._is_proper(x, db))]
+        propers = propers.to_frame().rename(columns={'normal_form': 'count'})
+        propers = propers.reset_index()
+        proper_rows = db.pymorphy[db.pymorphy.normal_form.isin(
+            propers['normal_form'])
+        ][['normal_form', 'gender']].drop_duplicates()
+        return propers.merge(proper_rows, on='normal_form')
 
     @staticmethod
     def _get_pronouns(morphology_df: pd.DataFrame):
@@ -16,19 +58,9 @@ class AntecedentCandidatesAlgorithm(NlpAlgorithm):
         pronouns_df = pronouns_df.reset_index()
         return pronouns_df.add_prefix('pronoun_')
 
-    # @staticmethod
-    # def _get_antecedent_candidates(morphology_df: pd.DataFrame):
-    #     antecedent_candidate_filter = morphology_df['POS'].isin(
-    #         ['NOUN', 'PRON', 'ADJF', 'ADJS', 'NPRO', 'PRCL', 'PRTF', 'PRTS',
-    #          'ADVB']) & (morphology_df['number'] == 'sing')
-    #     antecedent_candidates_df = morphology_df[antecedent_candidate_filter][
-    #         ['gender']]
-    #     antecedent_candidates_df = antecedent_candidates_df.reset_index()
-    #     return antecedent_candidates_df.add_prefix('candidate_')
-
-    @staticmethod
-    def _get_antecedent_candidates(db: DataBundle):
-        cand_filter = db.slovnet['POS'].isin(
+    def _get_antecedent_candidates(self, db: DataBundle):
+        slovnet = self._get_features(db, 'slovnet', self.slvnt)
+        cand_filter = slovnet['POS'].isin(
             ['NOUN', 'PROPN', 'ADJ', 'PRON', 'DET']) & (db.slovnet['Number'] == 'Sing')
         candidates_df = db.pymorphy[cand_filter][['gender']]
         return candidates_df[['gender']].reset_index().add_prefix('candidate_')
@@ -50,12 +82,16 @@ class AntecedentCandidatesAlgorithm(NlpAlgorithm):
                           'candidate_word_id',
                           'candidate_distance']].reset_index(drop=True)
 
+    @staticmethod
+    def _get_features(db: DataBundle, frames_name: str, featurizer: SimpleFeaturizer):
+        try:
+            return db.data_frames[frames_name]
+        except KeyError:
+            featurizer.featurize(db)
+            return db.data_frames[frames_name]
+
     def get_candidates(self, db: DataBundle):
-        pmf = PyMorphyFeaturizer()
-        pmf.featurize(db)
-        slvnt = SlovnetFeaturizer()
-        slvnt.featurize(db)
-        morphology_df = db.data_frames['pymorphy']
+        morphology_df = self._get_features(db, 'pymorphy', self.pmf)
         pronouns_df = self._get_pronouns(morphology_df)
         antecedent_candidates_df = self._get_antecedent_candidates(db)
         return self._filter_candidates(pronouns_df, antecedent_candidates_df)
@@ -65,7 +101,7 @@ class AntecedentCandidatesAlgorithm(NlpAlgorithm):
                            candidates_df: pd.DataFrame = None):
         if candidates_df is None:
             candidates_df = self.get_candidates(db)
-        slovnet = db['slovnet']
+        slovnet = self._get_features(db, 'slovnet', self.slvnt)
 
         parent_ids = slovnet[
             slovnet.index.isin(
@@ -75,6 +111,43 @@ class AntecedentCandidatesAlgorithm(NlpAlgorithm):
                      .rename(columns={'syntax_parent_id': 'pronoun_parent_id',
                                       'word_id': 'pronoun_word_id'}))
         return candidates_df.merge(parent_df, on='pronoun_word_id')
+
+    @staticmethod
+    def get_big_deviation_only(df: pd.DataFrame):
+        product_groups = \
+        df.drop_duplicates(['pronoun_parent_norm', 'candidate_word_norm'],
+                           keep='last').groupby(['pronoun_word_id'])['product']
+        maxes = product_groups.max()
+        means = product_groups.mean()
+        stds = product_groups.std()
+        max_indices = product_groups.idxmax().dropna().astype(int)
+        best_in_group = df.loc[max_indices]
+        temp = ((product_groups.count() < 4) | (
+                    stds < maxes - means)).to_frame()
+        return best_in_group[
+            best_in_group.pronoun_word_id.isin(temp[temp['product']].index)]
+
+    def filter_glove(self, with_parents_df: pd.DataFrame, pymorphy: pd.DataFrame):
+        product_df = with_parents_df.merge(
+            pymorphy.normal_form.rename('pronoun_parent_norm'),
+            left_on='pronoun_parent_id', right_index=True, how='left')
+        product_df = product_df.merge(
+            pymorphy.normal_form.rename('candidate_word_norm'),
+            left_on='candidate_word_id', right_index=True, how='left')
+        featurizer = NavecFeaturizer()
+        product_df['product'] = featurizer.get_glove_prod(product_df,
+                                                          'pronoun_parent_norm',
+                                                          'candidate_word_norm')
+        return self.get_big_deviation_only(product_df)
+
+    def run_full(self, db: DataBundle):
+        self.pmf.featurize(db)
+        db.pymorphy['normal_form'] = db.pymorphy['normal_form'].apply(
+            lambda x:
+            self.pronoun_replacer[x] if x in self.pronoun_replacer else x)
+        candidates_df = self.get_candidates(db)
+        with_parents_df = self.get_pronoun_parent(db, candidates_df)
+        return self.filter_glove(with_parents_df, db.pymorphy)
 
     def _run_inner(self, db: DataBundle, index: pd.Index):
         pass
