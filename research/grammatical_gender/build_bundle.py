471457d2dbcef628fe95eb34f3d65b55621d41fd
SergeyPishchulov <lcmrb26@bk.ru>
2022-11-18 15:14:42 +0600

    GG build bundle with single label

---
+++ research/grammatical_gender/build_bundle.py
@@ -0,0 +1,68 @@
+from tg.grammar_ru.ml.tasks.grammatical_gender.bundle import GGTrainIndexBuilder
+from tg.grammar_ru.ml.tasks.tsa.bundle import build_dictionary, TrainIndexBuilder
+from tg.grammar_ru.ml.corpus import CorpusReader, CorpusBufferedWriter, CorpusBuilder
+from tg.grammar_ru.ml.features import PyMorphyFeaturizer, SlovnetFeaturizer, SyntaxTreeFeaturizer, SyntaxStatsFeaturizer
+from tg.grammar_ru.common import Loc
+from tg.common import S3Handler
+from yo_fluq_ds import *
+from pathlib import Path
+
+# VOCAB_FILE = Path(__file__).parent / 'words.json'
+
+#
+# def read_data():
+#     return CorpusReader(Loc.corpus_path / 'lenta.base.zip').get_frames().feed(fluq.with_progress_bar())
+
+
+def build_index():
+    gg_index_builder = GGTrainIndexBuilder()
+    CorpusBuilder.transfuse_corpus(
+        [Loc.corpus_path / 'lenta.base.zip'],
+        Loc.bundles_path / 'grammatical_gender/prepare/raw/raw.zip',
+        selector=gg_index_builder.build_train_index
+    )
+
+
+def featurize_index():
+    CorpusBuilder.featurize_corpus(
+        Loc.bundles_path / 'grammatical_gender/prepare/raw/raw.zip',
+        Loc.bundles_path / 'grammatical_gender/prepare/feat/feat.zip',
+        [
+            PyMorphyFeaturizer(),
+            SlovnetFeaturizer(),
+            SyntaxTreeFeaturizer(),
+            SyntaxStatsFeaturizer()
+        ],
+        2,
+        True,
+    )
+
+
+def assemble(name, limit):
+    bundle_path = Loc.bundles_path / f'grammatical_gender/{name}'
+    CorpusBuilder.assemble(
+        Loc.bundles_path / 'grammatical_gender/prepare/feat/feat.zip',
+        bundle_path,
+        limit
+    )
+    src = pd.read_parquet(bundle_path / 'src.parquet')
+    index = GGTrainIndexBuilder.build_index_from_src(src)
+    index.to_parquet(bundle_path / 'index.parquet')
+    print(index.groupby('split').size())
+
+
+def upload_bundle(name):
+    bundle_path = Loc.bundles_path / f'tsa/{name}'
+    S3Handler.upload_folder(
+        'ps-data-science-sandbox',
+        'sagemaker/tsa/datasets/' + name,
+        bundle_path)
+
+
+if __name__ == '__main__':
+    build_index()
+    featurize_index()
+    assemble('toy', 5)
+    # assemble('big', 50)
+    # assemble('full', None)
+    # upload_bundle('big')
---
+++ tg/grammar_ru/ml/tasks/grammatical_gender/bundle.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+from tg.common import DataBundle
+from tg.common.ml.batched_training import train_display_test_split
+from tg.grammar_ru.ml.features import PyMorphyFeaturizer
+import pymorphy2
+
+
+class GGTrainIndexBuilder:
+
+    def __init__(self):
+        self.pmf = PyMorphyFeaturizer()
+        self.an = pymorphy2.MorphAnalyzer()  # TODO delete
+        self.speech_part_labels = ['NOUN', 'ADJF', 'ADJS', 'VERB', 'PRTF', 'PRTS']  # TODO what else?
+        self.gender_nums = {g: i for i, g in enumerate(['masc', 'femn', 'neut', 'nan'])}
+
+    def build_train_index(self, df):
+        df['label'] = -1
+        db = DataBundle(src=df)
+        self.pmf.featurize(db)
+        morphed = db.data_frames['pymorphy']
+        morphed.replace({np.nan: 'nan'}, inplace=True)
+        df['is_target'] = morphed.POS.isin(self.speech_part_labels)
+        df.loc[df.is_target, 'label'] = morphed[df.is_target].gender.replace(self.gender_nums)
+        return [df]
+
+    @staticmethod
+    def build_index_from_src(src_df):
+        df = src_df.loc[src_df.is_target][['word_id', 'sentence_id', 'label']].copy()
+        df = df.reset_index(drop=True)
+        df.index.name = 'sample_id'
+        df['split'] = train_display_test_split(df)
+        return df
