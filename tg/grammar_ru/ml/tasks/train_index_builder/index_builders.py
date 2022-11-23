41fa2273ef5ec160ac35ae4905185dea0026ccd3
invis166 <kolesnikovv028@gmail.com>
2022-11-23 13:11:53 +0500

    chtoby index builder

--- tg/grammar_ru/ml/tasks/train_index_builder/index_builders.py
+++ tg/grammar_ru/ml/tasks/train_index_builder/index_builders.py
@@ -1,5 +1,4 @@
 import typing as tp
-import re
 
 import numpy as np
 import pandas as pd
@@ -36,8 +35,8 @@ class NNnIndexBuilder(DictionaryIndexBuilder):
             word_normalizer: WordNormalizer,
             add_negative_samples: bool = True):
         super().__init__(
-            good_words=good_words,
             add_negative_samples=add_negative_samples)
+        self.good_words = good_words
         self._word_normalizer = word_normalizer
 
     def _get_targets(self, df: pd.DataFrame) -> pd.Series:
@@ -61,3 +60,54 @@ class NNnIndexBuilder(DictionaryIndexBuilder):
 
     def _get_normalized_word(self, word: str) -> str:
         return self._word_normalizer.normalize_word(word)
+
+
+class ChtobyIndexBuilder(DictionaryIndexBuilder):
+    def __init__(self, add_negative_samples: bool = True):
+        super().__init__(add_negative_samples=add_negative_samples)
+        self.good_words = ['чтобы', 'что бы']
+
+    def _get_targets(self, df: pd.DataFrame) -> pd.Series:
+        return df.word.str.lower().isin(self.good_words)
+
+    def _get_another(self, word: str) -> str:
+        if word == 'чтобы':
+            return 'что бы'
+        elif word == 'что бы':
+            return 'чтобы'
+
+        return word
+
+    def _build_negative_from_positive(self, positive: pd.DataFrame) -> pd.DataFrame:
+        negative = positive.copy()
+        negative.word = np.where(
+            ~negative.is_target,
+            negative.word,
+            np.where(
+                negative['word'].apply(lambda word: word in ('чтобы', 'что бы')),
+                negative['word'].apply(self._get_another),
+                negative['word'].apply(self._get_another)
+            )
+        )
+
+        negative['label'] = 1
+
+        return negative
+
+    @staticmethod
+    def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+        # transforming 'что' + 'бы' to 'что бы'
+        result = df.copy()
+        what = result[result['word'] == 'что']
+        what_next = what[['sentence_id', 'word_index']].copy()
+        what_next['word_index'] += 1
+        what_neighbour = result.merge(what_next, on=['sentence_id', 'word_index'], how='inner')
+        would = what_neighbour[what_neighbour['word'] == 'бы']
+
+        if would.shape[0] != 0:
+            what_with_pair_loc = would['word_id']
+            result.loc[what_with_pair_loc - 1, 'word'] = 'что бы'
+            result.loc[what_with_pair_loc - 1, 'word_length'] += 3
+
+            # word_index now is inconsistent
+            return result.drop(what_with_pair_loc)
