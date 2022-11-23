f8bad8ffdd9a78f35b69050485d541dd586943c6
Alexandra Zaytseva <alexandrazaitzewa@yandex.ru>
2022-11-23 22:29:28 +0500

    Bundle but cringe

--- tg/grammar_ru/ml/features/__init__.py
+++ tg/grammar_ru/ml/features/__init__.py
@@ -1,4 +1,4 @@
-from .antecedent_featurizer import AntecedentFeaturizer
+from .distances_featurizer import DistancesFeaturizer
 from .architecture import Featurizer, SimpleFeaturizer
 from .pymorphy_featurizer import PyMorphyFeaturizer
 from .slovnet_featurizer import SlovnetFeaturizer
