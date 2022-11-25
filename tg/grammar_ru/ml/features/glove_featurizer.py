486cedaab9f39c6c19b2c45227d56f98e48b808d
Yuri Okulovsky <yuri.okulovsky@gmail.com>
2022-11-25 22:23:04 +0100

    glove featurizer now has more settings

--- tg/grammar_ru/ml/features/glove_featurizer.py
+++ tg/grammar_ru/ml/features/glove_featurizer.py
@@ -15,25 +15,40 @@ def download_dependency(fname, url, disable_downloading):
 
 
 class GloveFeaturizer(Featurizer):
-    def __init__(self, disable_downloading = False):
+    def __init__(self,
+                 disable_downloading = False,
+                 add_lowercase = True,
+                 add_normal_form = True
+                 ):
         download_dependency('glove_featurizer_navec', 'https://storage.yandexcloud.net/natasha-navec/packs/navec_news_v1_1B_250K_300d_100q.tar', disable_downloading)
         self.navec = Navec.load(Loc.data_cache_path / 'glove.tar')
         self.words = list(self.navec.vocab.words)
         self.ndf = pd.DataFrame(dict(word=self.words)).reset_index(drop=False).set_index('word').rename(columns={'index': 'glove_index'})
+        self.add_lowercase = add_lowercase
+        self.add_normal_form = add_normal_form
 
     def get_frame_names(self) -> List[str]:
         return ['glove_keys','glove_scores']
 
     def featurize(self, db: DataBundle) -> None:
         df = db.src.set_index('word_id')[['word', 'word_type']]
-        df['lowercase_word'] = df.word.str.lower()
-        df = df.merge(db.pymorphy[['normal_form']], left_index=True, right_index=True)
+        check_columns = ['word']
+
+        if self.add_lowercase:
+            df['lowercase_word'] = df.word.str.lower()
+            check_columns.append('lowercase_word')
+
+        if self.add_normal_form:
+            df = df.merge(db.pymorphy[['normal_form']], left_index=True, right_index=True)
+            check_columns.append('normal_form')
+
+
         df = df.loc[df.word_type == 'ru'].copy()
 
         UNK = '<unk>'
         df['selected_word'] = UNK
         df['selected_word_column'] = UNK
-        for column in ['word', 'lowercase_word', 'normal_form']:
+        for column in check_columns:
             lc = (df.selected_word == UNK) & df[column].isin(self.words)
             df.loc[lc, 'selected_word'] = df.loc[lc][column]
             df.loc[lc, 'selected_word_column'] = column
