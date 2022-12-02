6258180d18ee23f96cedf2b4f84845fb69ea5688
invis166 <kolesnikovv028@gmail.com>
2022-12-02 12:14:13 +0500

    change chtoby index builder

--- tg/grammar_ru/ml/tasks/train_index_builder/index_builders.py
+++ tg/grammar_ru/ml/tasks/train_index_builder/index_builders.py
@@ -6,9 +6,14 @@ import pandas as pd
 from .train_index_builder import DictionaryIndexBuilder
 from ..n_nn.word_normalizer import WordNormalizer
 from ..n_nn.regular_expressions import single_n_regex
+from ....common import Separator
 
 
 class TsaIndexBuilder(DictionaryIndexBuilder):
+    def __init__(self, good_words: tp.Sequence[str], add_negative_samples: bool = True) -> None:
+        self.good_words = good_words
+        super().__init__(add_negative_samples=add_negative_samples)
+
     def _get_targets(self, df: pd.DataFrame) -> pd.Series:
         return df.word.str.lower().isin(self.good_words)
 
@@ -67,105 +72,40 @@ class ChtobyIndexBuilder(DictionaryIndexBuilder):
         super().__init__(add_negative_samples=add_negative_samples)
         self.good_words = ['чтобы', 'что бы']
 
-    def build_train_index(self, df: pd.DataFrame) -> tp.List[pd.DataFrame]:
-        preprocessed = ChtobyIndexBuilder.preprocess(df)
+    def _get_targets(self, df: pd.DataFrame) -> tp.Sequence[bool]:
+        targets = df.set_index('word_id').word.str.lower() == 'чтобы'
 
-        return super().build_train_index(preprocessed)
+        chto = df[df['word'] == 'что']
+        chto_next = chto['word_id'] + 1
+        chto_neighbour = df.merge(chto_next, how='inner')
+        by = chto_neighbour[chto_neighbour['word'] == 'бы']
 
-    def _get_targets(self, df: pd.DataFrame) -> pd.Series:
-        return df.word.str.lower().isin(self.good_words)
+        targets[by['word_id'] - 1] = True
 
-    def _get_another(self, word: str) -> str:
-        if word == 'чтобы':
-            return 'что бы'
-        elif word == 'что бы':
-            return 'чтобы'
-
-        return word
+        return targets.values
 
     def _build_negative_from_positive(self, positive: pd.DataFrame) -> pd.DataFrame:
         negative = positive.copy()
-        negative.word = np.where(
-            ~negative.is_target,
-            negative.word,
-            np.where(
-                negative['word'] == 'чтобы',
-                negative['word'].str.replace('чтобы', 'что бы'),
-                negative['word'].str.replace('что бы', 'чтобы')
-            )
-        )
+        chtoby = negative['word'] == 'чтобы'
+
+        # transforming 'что' + 'бы' to 'чтобы'
+        chto = negative[negative['word'] == 'что']
+        chto_next = chto[['sentence_id', 'word_index']].copy()
+        chto_next['word_index'] += 1
+        chto_neighbor = negative.merge(chto_next, on=['sentence_id', 'word_index'], how='inner')
+        by = chto_neighbor[chto_neighbor['word'] == 'бы']
+
+        if by.shape[0]:
+            chto_with_pair_loc = by['word_id']
+            negative.loc[chto_with_pair_loc - 1, 'word'] = 'чтобы'
+
+        # transforming 'чтобы' to 'что' + 'бы'
+        negative.loc[chtoby, 'word'] = 'что бы'
+        if by.shape[0]:
+            negative = negative.drop(chto_with_pair_loc)
+        negative = Separator.separate_string(Separator.Viewer().to_text(negative))
+        negative['is_target'] = self._get_targets(negative)
 
         negative['label'] = 1
 
         return negative
-
-    @staticmethod
-    def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-        # transforming 'что' + 'бы' to 'что бы'
-        result = df.copy()
-        what = result[result['word'] == 'что']
-        what_next = what[['sentence_id', 'word_index']].copy()
-        what_next['word_index'] += 1
-        what_neighbour = result.merge(what_next, on=['sentence_id', 'word_index'], how='inner')
-        would = what_neighbour[what_neighbour['word'] == 'бы']
-
-        if would.shape[0] != 0:
-            what_with_pair_loc = would['word_id']
-            result.loc[what_with_pair_loc - 1, 'word'] = 'что бы'
-            result.loc[what_with_pair_loc - 1, 'word_length'] += 3
-
-            # word_index now is inconsistent
-            return result.drop(what_with_pair_loc)
-
-        return result
-
-
-class TakzheIndexBuilder(DictionaryIndexBuilder):
-    def __init__(self, add_negative_samples: bool = True):
-        super().__init__(add_negative_samples=add_negative_samples)
-        self.good_words = ['также', 'так же']
-
-    def _get_targets(self, df: pd.DataFrame) -> pd.Series:
-        return df.word.str.lower().isin(self.good_words)
-
-    def _get_another(self, word: str) -> str:
-        if word == 'также':
-            return 'так же'
-        elif word == 'так же':
-            return 'также'
-
-        return word
-
-    def _build_negative_from_positive(self, positive: pd.DataFrame) -> pd.DataFrame:
-        negative = positive.copy()
-        negative.word = np.where(
-            ~negative.is_target,
-            negative.word,
-            np.where(
-                negative['word'].apply(lambda word: word in self.good_words),
-                negative['word'].apply(self._get_another),
-                negative['word'].apply(self._get_another)
-            )
-        )
-
-        negative['label'] = 1
-
-        return negative
-
-    @staticmethod
-    def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-        # transforming 'так' + 'же' to 'так же'
-        result = df.copy()
-        tak = result[result['word'] == 'так']
-        tak_next = tak[['sentence_id', 'word_index']].copy()
-        tak_next['word_index'] += 1
-        tak_neighbour = result.merge(tak_next, on=['sentence_id', 'word_index'], how='inner')
-        would = tak_neighbour[tak_neighbour['word'] == 'же']
-
-        if would.shape[0] != 0:
-            tak_with_pair_loc = would['word_id']
-            result.loc[tak_with_pair_loc - 1, 'word'] = 'так же'
-            result.loc[tak_with_pair_loc - 1, 'word_length'] += 3
-
-            # word_index now is inconsistent
-            return result.drop(tak_with_pair_loc)
