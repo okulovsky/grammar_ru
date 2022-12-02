eb8f1fc765856dfd55ef369cdc44163b00c0236c
invis166 <kolesnikovv028@gmail.com>
2022-12-02 12:12:21 +0500

    cli for bundle building and training

--- research/alternative/chtoby/build_bundle.py
+++ research/alternative/chtoby/build_bundle.py
@@ -1,23 +1,42 @@
+import dataclasses
+import click
+
 from research.common import build_bundle
 from tg.grammar_ru.common import Loc
 from tg.grammar_ru.ml.tasks.train_index_builder.index_builders import ChtobyIndexBuilder
 
 
-index_path = Loc.bundles_path/'chtoby/prepare/raw/raw.zip'
-if not (index_path).exists():
-    print('Building index')
+@dataclasses.dataclass
+class DataPath:
+    INDEX_PATH = Loc.bundles_path/'chtoby/prepare/raw/raw.zip'
+    FEATURES_PATH = Loc.bundles_path/'chtoby/prepare/feat/feat.zip'
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command()
+def index() -> None:
+    print(f'Building index in {DataPath.INDEX_PATH}')
     index_builder = ChtobyIndexBuilder()
-    build_bundle.build_index(index_builder, build_bundle.LENTA_CORPUS_PATH, index_path)
+    build_bundle.build_index(index_builder, build_bundle.LENTA_CORPUS_PATH, DataPath.INDEX_PATH)
 
 
-features_path = Loc.bundles_path/'chtoby/prepare/feat/feat.zip'
-if not (features_path).exists():
-    print('Extracting features')
-    build_bundle.featurize_index(index_path, features_path, workers=3)
+@cli.command()
+def features() -> None:
+    print(f'Extracting features in {DataPath.FEATURES_PATH}')
+    build_bundle.featurize_index(DataPath.INDEX_PATH, DataPath.FEATURES_PATH, workers=3)
 
 
-bundle_name = 'big'
-bundle_path = Loc.bundles_path/f'chtoby/{bundle_name}'
-if not (bundle_path).exists():
-    print('Building bundle')
-    build_bundle.assemble(200, features_path, bundle_path)
+@cli.command()
+def bundle() -> None:
+    bundle_name = 'toy'
+    bundle_path = Loc.bundles_path/f'chtoby/{bundle_name}'
+    print(f'Building bundle in {bundle_path}')
+    build_bundle.assemble(50, DataPath.FEATURES_PATH, bundle_path)
+
+
+if __name__ == '__main__':
+    cli()
--- research/alternative/chtoby/training.py
+++ research/alternative/chtoby/training.py
@@ -1,10 +1,18 @@
 from pathlib import Path
+import click
 
 from tg.grammar_ru.common import Loc
 from research.common import run_training
 
 
-bundle_name = 'big'
-bundle_path = Loc.bundles_path/f'chtoby/{bundle_name}'
+@click.command()
+@click.argument('bundle_name')
+def cli(bundle_name: str) -> None:
+    bundle_path = Loc.bundles_path/f'chtoby/{bundle_name}'
 
-run_training.run_local(Path(__file__)/bundle_path)
+    run_training.run_local(Path(__file__)/bundle_path)
+
+
+if __name__ == '__main__':
+    cli()
+
