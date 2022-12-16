15d3b375dba8062f6fb8b9f38274fe5684ed444d
invis166 <kolesnikovv028@gmail.com>
2022-12-16 15:29:27 +0500

    rename imports

--- research/common/build_bundle.py
+++ research/common/build_bundle.py
@@ -9,8 +9,8 @@ from tg.grammar_ru.common import Loc
 from tg.grammar_ru.ml.features import PyMorphyFeaturizer, SlovnetFeaturizer, SyntaxTreeFeaturizer, SyntaxStatsFeaturizer
 from tg.grammar_ru.ml.corpus import CorpusReader, CorpusBuilder
 
-from tg.grammar_ru.ml.tasks.train_index_builder.index_builders import DictionaryIndexBuilder
-from tg.grammar_ru.ml.tasks.train_index_builder.index_builders import NNnIndexBuilder
+from tg.grammar_ru.ml.tasks.train_index_builder.index_builders import IndexBuilder
+from tg.grammar_ru.ml.tasks.train_index_builder.sentence_filterer import SentenceFilterer
 
 
 LENTA_CORPUS_PATH = Loc.corpus_path/'lenta.base.zip'
@@ -22,8 +22,22 @@ def read_data(corpus_path: Path) -> tp.Iterable[pd.DataFrame]:
             .feed(yfds.fluq.with_progress_bar()))
 
 
+def filter_corpus(
+        filterer: SentenceFilterer,
+        corpus_path: Path,
+        filtered_corpus_path: Path,
+        word_limit: tp.Optional[int] = None
+        ) -> None:
+    CorpusBuilder.transfuse_corpus(
+        [corpus_path],
+        filtered_corpus_path,
+        words_limit=word_limit,
+        selector=filterer.select
+    )
+
+
 def build_index(
-        index_builder: DictionaryIndexBuilder,
+        index_builder: IndexBuilder,
         corpus_path: Path,
         bundle_path: Path,
         word_limit: tp.Optional[int] = None
@@ -32,7 +46,7 @@ def build_index(
         [corpus_path],
         bundle_path,
         words_limit=word_limit,
-        selector=index_builder.build_train_index
+        selector=index_builder.select
     )
 
 
@@ -62,6 +76,6 @@ def assemble(
         limit
     )
     src = pd.read_parquet(bundle_path/'src.parquet')
-    index = NNnIndexBuilder.build_index_from_src(src)
+    index = IndexBuilder.build_index_from_src(src)
     index.to_parquet(bundle_path/'index.parquet')
     print(index.groupby('split').size())
