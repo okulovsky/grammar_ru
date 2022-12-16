e5ebe93b694abcf175c1ecc21d1f3f5f3fd2845e
invis166 <kolesnikovv028@gmail.com>
2022-12-16 15:30:09 +0500

    add ITransfuseSelector to __init__.py

--- tg/grammar_ru/ml/corpus/__init__.py
+++ tg/grammar_ru/ml/corpus/__init__.py
@@ -4,3 +4,4 @@ from .corpus_builder import CorpusBuilder
 from .control_distribution import ControlDistribution
 from .corpus_buffered_writer import CorpusBufferedWriter
 from .corpus_balancer import BucketCorpusBalancer
+from .transfuse_selector import ITransfuseSelector
