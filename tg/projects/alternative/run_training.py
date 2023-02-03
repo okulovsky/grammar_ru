175d1d35a420e5873f51b0d683743cdb06c13bc5
Yuri Okulovsky <yuri.okulovsky@gmail.com>
2023-02-03 13:11:56 +0100

    WIP

--- tg/projects/alternative/run_training.py
+++ tg/projects/alternative/run_training.py
@@ -25,11 +25,12 @@ def remote_run(**kwargs):
         routine.remote().execute()
 
 
+
 if __name__ == '__main__':
     pass
     #debug_run(False)
     #remote_run(epoch_count=[3], batch_size=[10000, 20000, 50000, 100000, 200000])
-    #remote_run(context_length=[9, 15, 21, 25], batch_size=[100000], epoch_count=[100], features=['p', None])
-    remote_run(hidden_size=[10, 20, 50, 100], learning_rate=[0.05, 0.02, 0.01])
+    #remote_run(hidden_size=[10, 20, 50, 100], learning_rate=[0.05, 0.02, 0.01])
+    remote_run(hidden_size=[100], learning_rate=[0.01], context_length=[27], features=['p', None])
 
 
