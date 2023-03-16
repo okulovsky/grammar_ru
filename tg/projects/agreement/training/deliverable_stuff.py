226b3bf2fb6eba218ce5272794b8d8a36fc26442
SergeyPishchulov <lcmrb26@bk.ru>
2023-03-17 01:29:05 +0500

    deliverable stuff fix imports

--- tg/projects/agreement/training/deliverable_stuff.py
+++ tg/projects/agreement/training/deliverable_stuff.py
@@ -1,26 +1,48 @@
+from typing import List, Union
+from pathlib import Path
+from dotenv import load_dotenv
+from yo_fluq_ds import fluq
+import torch
+import pandas as pd
+
 from ....common.ml.training_core import TrainingEnvironment
 from ....common.ml.batched_training import IndexedDataBundle
 from ....common.ml import batched_training as bt
 from ....common.ml import dft
 from ....grammar_ru.components import CoreExtractor, PlainContextBuilder
-
-# from ..components.plain_context_builder import PlainContextBuilder
-# from ...components.training_task_factory import TaskFactory
-# from ...components.training_task_factory import Conventions
 from ....common.ml.batched_training.factories import Conventions, FeedForwardNetwork
 from ....common.ml.batched_training.context import ContextualAssemblyPoint
-
-# from ...components.contextual_binding import ContextualBinding, ContextualNetworkType
-# from .....common.ml.batched_training.torch.networks import FeedForwardNetwork, FullyConnectedNetwork
-from pathlib import Path
-from dotenv import load_dotenv
-from yo_fluq_ds import fluq
-import datetime
-import torch
-from typing import Dict, Optional  # TODO delete redundant
-
-
+from ....common.ml.batched_training import factories as btf
+from ....common.ml import batched_training as bt
+from ....common.ml import dft
 from ....common import Loc
+from ....common.ml import batched_training as bt
+from ....common.ml.batched_training import context as btc
+from ....grammar_ru.components import CoreExtractor
+
+
+def create_assembly_point(context_length=6):
+    ap = btc.ContextualAssemblyPoint(
+        name='features',
+        context_builder=PlainContextBuilder(
+            include_zero_offset=True,
+            left_to_right_contexts_proportion=0.5
+        ),
+        extractor=CoreExtractor(join_column='another_word_id'),
+        context_length=context_length
+    )
+    ap.reduction_type = ap.reduction_type.Dim3Folded
+    return ap
+
+
+def get_multilabel_extractor():
+    label_extractor = (bt.PlainExtractor
+                       .build(btf.Conventions.LabelFrame)
+                       .index()
+                       .apply(take_columns=['label'],
+                              transformer=dft.DataFrameTransformerFactory.default_factory())
+                       )
+    return label_extractor
 
 
 class MulticlassMetrics(bt.Metric):
@@ -68,77 +90,74 @@ class MulticlassMetrics(bt.Metric):
         return result
 
 
-def get_multilabel_extractor():
-    label_extractor = (bt.PlainExtractor
-                       .build(Conventions.LabelFrame)
-                       .index()
-                       .apply(take_columns=['label'],
-                              transformer=dft.DataFrameTransformerFactory.default_factory())
-                       )
-    return label_extractor
+def _update_sizes_with_argument(argument_name, argument, sizes, modificator):
+    if argument is None:
+        return sizes
+    elif isinstance(argument, torch.Tensor):
+        return modificator(sizes, argument.shape[1])
+    elif isinstance(argument, pd.DataFrame):
+        return modificator(sizes, argument.shape[1])
+    elif isinstance(argument, int):
+        return modificator(sizes, argument)
+    else:
+        raise ValueError(
+            f"Argument {argument_name} is supposed to be int, Tensor or none, but was `{argument}`")
 
 
-class MyNetworkFactory:
-    def __init__(self, nn_head_factory):
-        self.nn_head_factory = nn_head_factory
+class FullyConnectedNetwork(torch.nn.Module):
+    def __init__(self,
+                 sizes: List[int],
+                 input: Union[None, torch.Tensor, int] = None,
+                 output: Union[None, torch.Tensor, int] = None):
+        super(FullyConnectedNetwork, self).__init__()
+        sizes = _update_sizes_with_argument(
+            'input', input, sizes, lambda s, v: [v] + s)
+        sizes = _update_sizes_with_argument(
+            'output', output, sizes, lambda s, v: s + [v])
+        self.layers = torch.nn.ModuleList()
+        for i in range(len(sizes) - 1):
+            self.layers.append(torch.nn.Linear(sizes[i], sizes[i + 1]))
 
-    def create_network(self, task, input):  # input is batch ~ sample
-        nn_head = self.nn_head_factory.create_network(task=None, input=input)
-        head_out = nn_head(input)
-        hidden_size = head_out.shape[1]
-        labels_count = input['label'].shape[1]
-        nn_tail = FullyConnectedNetwork(
-            sizes=[hidden_size], input=hidden_size, output=labels_count)
-        # return FeedForwardNetwork(nn_head, nn_tail, torch.nn.Softmax(dim=1))
-        return FeedForwardNetwork(nn_head, nn_tail)
-
-
-class ClassificationTask(TaskFactory):
-    def get_network(self, _, sample):
-        assembled_network_factory = MyNetworkFactory(self.nn_head_factory)
-        assembled_network = assembled_network_factory.create_network(
-            task=None, input=sample)
-        return assembled_network
-
-    def create_task(
-        self, data: IndexedDataBundle,
-            env: Optional[TrainingEnvironment] = None) -> None:
-
-        metrics = bt.MetricPool().add(MulticlassMetrics())
-        self.instantiate_default_task(
-            epoch_count=1,
-            batch_size=30_000,
-            mini_batch_size=50,
-            mini_epoch_count=1,
-            metric_pool=metrics
-        )
-        # self.task.settings.evaluation_batch_limit = 5  # TODO comment
-        # self.task.settings.training_batch_limit = 5
-
-        plain_context_builder = PlainContextBuilder(include_zero_offset=False,
-                                                    left_to_right_contexts_proportion=0.5)
-
-        plain_context = ContextualAssemblyPoint(
-            name='plain_context',
-            context_length=3,
-            network_type=ContextualNetworkType.Plain,
-            hidden_size=[],
-            context_builder=plain_context_builder,
-            extractor=CoreExtractor(join_column='another_word_id'),
-            debug=False
-        )
-        self.nn_head_factory = plain_context.create_network_factory(
-            task=None, input=None)  # TODO could be better?
-        core_extractor = plain_context.create_extractor(task=None, bundle=data)
-        self.setup_batcher(data, [core_extractor, get_multilabel_extractor()])
-        # ,optimizer_ctor='torch.optim:Adam')
-        self.setup_model(self.get_network, learning_rate=0.1)
+    def forward(self, input):
+        X = input
+        for layer in self.layers:
+            X = layer(X)
+            X = torch.sigmoid(X)
+        return X
 
 
-class ClassificationTaskByBatchSize(ClassificationTask):
-    def __init__(self, batch_size):
-        super().__init__()
-        self.batch_size = batch_size
+class Network(torch.nn.Module):
+    def __init__(self, head, hidden_size, batch):
+        super(Network, self).__init__()
+        self.head = head
+        self.tail = FullyConnectedNetwork(
+            sizes=[], input=hidden_size, output=batch.index_frame.label.nunique())
 
-    def instantiate_default_task(self, epoch_count, batch_size, metric_pool, mini_batch_size=200, mini_epoch_count=8):
-        return super().instantiate_default_task(1, self.batch_size, metric_pool, mini_batch_size, mini_epoch_count)
+    def forward(self, batch):
+        return self.tail(self.head(batch))
+
+
+class NetworkFactory:
+    def __init__(self, assembly_point):
+        self.assembly_point = assembly_point
+
+    def __call__(self, batch):
+        head_factory = self.assembly_point.create_network_factory()
+        head = head_factory(batch)
+        return Network(head, self.assembly_point.hidden_size,  batch)
+
+
+class TrainingTask(btf.TorchTrainingTask):
+    def __init__(self):
+        super(TrainingTask, self).__init__()
+        self.metric_pool = bt.MetricPool().add(MulticlassMetrics())
+        self.features_ap = create_assembly_point()
+
+    def initialize_task(self, idb):
+        ap = create_assembly_point()
+        ap.hidden_size = 50
+        ap.dim_3_network_factory.network_type = btc.Dim3NetworkType.LSTM
+        head_factory = ap.create_network_factory()
+        self.setup_batcher(
+            idb, [ap.create_extractor(), get_multilabel_extractor()])
+        self.setup_model(NetworkFactory(ap), ignore_consistancy_check=True)
