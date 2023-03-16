c13294391002fb638528032f39b284a58683500a
SergeyPishchulov <lcmrb26@bk.ru>
2023-03-17 01:04:23 +0500

    amazing success. net is launched

--- tg/projects/agreement/build_bundle.py
+++ tg/projects/agreement/build_bundle.py
@@ -7,14 +7,17 @@ from tg.grammar_ru.corpus import CorpusBuilder
 from tg.projects.agreement.bundle import AdjAgreementTrainIndexBuilder
 
 
-INDEXED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/raw/raw.zip'
-FEATURIZED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/feat/feat.zip'
+# INDEXED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/raw/raw.zip'
+INDEXED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/raw/raw_tiny.zip'
+# FEATURIZED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/feat/feat.zip'
+FEATURIZED_BUNDLE_PATH = Loc.bundles_path / 'agreement/prepare/books&pub_60K_balanced/feat/feat_tiny.zip'
 
 
 def build_index():
     index_builder = AdjAgreementTrainIndexBuilder()
     CorpusBuilder.transfuse_corpus(
-        [Loc.corpus_path / 'prepare/balanced/books&pub_60K_balanced_feat.zip'],
+        # [Loc.corpus_path / 'prepare/balanced/books&pub_60K_balanced_feat.zip'],
+        [Loc.corpus_path/"prepare/balanced/books&pub_tiny_balanced.zip"],
         INDEXED_BUNDLE_PATH,
         selector=index_builder
     )
@@ -36,7 +39,7 @@ def featurize_index():
 
 
 def assemble(name, limit):
-    bundle_path = Loc.bundles_path / f'agreement/{name}'
+    bundle_path = Loc.bundles_path / f'agreement/tiny_{name}'
     CorpusBuilder.assemble(
         FEATURIZED_BUNDLE_PATH,
         bundle_path,
@@ -61,6 +64,6 @@ if __name__ == '__main__':
     # featurize_index()
     # assemble('toy', 5)
     # assemble('mid', 20)
-    assemble('big', 100)
+    # assemble('big', 100)
     assemble('full', None)
     # upload_bundle('big')
