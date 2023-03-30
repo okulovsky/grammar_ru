8b6e3cf0305388b4a25c95564786347851018760
invis166 <kolesnikovv028@gmail.com>
2023-03-30 13:54:52 +0500

    fix data leak in context builder

--- tg/projects/punct/context_builder.py
+++ tg/projects/punct/context_builder.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class PunctContextBuilder(PlainContextBuilder):
-    def build_context(self, ibundle: bt.IndexedDataBundle, context_size: int) -> pd.DataFrame:
+    def build_context(self, ibundle: bt.IndexedDataBundle, context_size) -> pd.DataFrame:
         context_df = super().build_context(ibundle, context_size)
         context_df = context_df.reset_index()
         word_is_target = (
@@ -13,6 +13,8 @@ class PunctContextBuilder(PlainContextBuilder):
             .loc[context_df.sample_id].is_target
             .values
         )
-        context_df = context_df[~(word_is_target & (context_df.offset == 1))]
+        max_offset = context_df.offset.max()
+        rows_to_drop = (~(word_is_target & (context_df.offset == 1))) & ~(~word_is_target & (context_df.offset == max_offset))
+        context_df = context_df[rows_to_drop]
 
         return context_df.set_index(['sample_id', 'offset'])
