61e7e2ea87a90e1f088e2cc88473ae82a6575a4b
nikolaisilaev <nikolaisilaev@yandex-team.ru>
2023-12-19 23:02:55 +0300

    added working alternative

--- tg/grammar_ru/algorithms/alternative.py
+++ tg/grammar_ru/algorithms/alternative.py
@@ -13,34 +13,51 @@ from tg.projects.alternative import EndingNegativeSampler
 
 from tg.common import Loc
 from tg.grammar_ru.algorithms.architecture import NlpAlgorithm
-from tg.common.ml.batched_training import DataBundle, train_display_test_split
+from tg.common.ml.batched_training import DataBundle, train_display_test_split, IndexedDataBundle
 from tg.projects.alternative import DictionaryFilterer
 from tg.common.ml import batched_training as bt
 
-class AlternativeAlgorithm(NlpAlgorithm):
 
+class AlternativeAlgorithm(NlpAlgorithm):
     def __init__(self):
         super().__init__()
 
     def create_db(self, text: str):
         text_db = Separator.build_bundle(text, [SnowballFeaturizer()])
+        # text_db.index = text_db.src[['sentence_id', 'word']]
         return text_db
 
-    def create_index(self, db: DataBundle, text: str):
-        index = Separator.separate_string(text)
-        good_words = set(FileIO.read_json(Loc.files_path / 'tsa-dict.json'))
-        tsa_filter = DictionaryFilterer(good_words)
-        index = tsa_filter.filter(index).index
-        return index
+    def create_index(self, db: DataBundle, text: str) -> pd.DataFrame:
+        # Read the source data
+        src_df = db.src
+
+        # Filter words ending with 'тся' or 'ться'
+        regex = r'.*ться$|.*тся$'
+        filtered_df = src_df[src_df['word'].str.contains(regex)]
+
+        # Construct the index frame
+        index_frame = filtered_df[['word_id', 'sentence_id']].copy()
+        index_frame['label'] = -1  # Or use a placeholder value if needed
+        index_frame['error'] = 0
+        index_frame = index_frame.reset_index(drop=True)
+        index_frame.index.name = 'sample_id'
+
+        return index_frame
 
     def _run_inner(self, db: DataBundle, index: pd.Index) -> Optional[pd.DataFrame]:
-        idb = bt.IndexedDataBundle(
-            index_frame=index.to_frame(),
+        model_path = Loc.model_path / 'alternative_task.pickle'
+        idb = IndexedDataBundle(
+            index_frame=index,
             bundle=db
         )
-        model_path = Loc.model_path / 'alternative_task.pickle'
         with open(model_path, 'rb') as handle:
-            model = pickle.load(handle)
+            output = pickle.load(handle)
+            model = output
+            # model = output['training_task']
+            # batcher = output['batcher']
+            # self.model = model
+            # self.batcher = batcher
+            # print('ok')
 
         return model.predict(idb)
 
--- tg/grammar_ru/components/api/alternative_runner.py
+++ tg/grammar_ru/components/api/alternative_runner.py
@@ -12,7 +12,7 @@ from tg.common.ml.batched_training import sandbox as bts
 
 def main():
     alternative_algorithm = alternative.AlternativeAlgorithm()
-    text = 'Никуда не годится если нет воды напится'
+    text = 'Никуда не годится если нет. Пить ходить гулять водиться. Нечем больше поделиться. воды напится сражаться красоваться метается вылиться красится нравится.'
     db = alternative_algorithm.create_db(text)
     index = alternative_algorithm.create_index(db, text)
     result = alternative_algorithm.run(db, index)
