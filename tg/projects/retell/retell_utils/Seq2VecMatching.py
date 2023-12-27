78123c2ece17fa9347a4b20132251f8506ef50b9
mixailkys <9041758286@mail.ru>
2023-12-27 14:50:20 +0500

    add test for Seq2VecMatching.py -> fix some bugs in it

--- tg/projects/retell/retell_utils/Seq2VecMatching.py
+++ tg/projects/retell/retell_utils/Seq2VecMatching.py
@@ -8,50 +8,60 @@ from collections import defaultdict
 
 
 class Seq2VecConverter:
-    def __init__(self, model_name='distiluse-base-multilingual-cased', separator: AbstractSeparator = SeparatorRu):
+    def __init__(self, model_name='distiluse-base-multilingual-cased', separator=SeparatorRu):
         self.model = SentenceTransformer(model_name)
         self.viewer = separator.Viewer()
+        self._current_group_flag = 1
+        self._next_group_flag = 2
 
-    def get_matches(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> Dict[str, str]:
+    def get_matches(self, df_1: pd.DataFrame, df_2: pd.DataFrame, need_matching_dict=False):
         text_1, text_2 = [self.viewer.to_sentences_strings(df).values for df in [df_1, df_2]]
         encoded_text_1, encoded_text_2 = [self.model.encode(text, convert_to_numpy=True) for text in [text_1, text_2]]
         cos_sim = util.cos_sim(encoded_text_1, encoded_text_2)
-        matching = self._get_monotone_matchings(cos_sim)
-        matched_sent = {text_1[t_1_id]: text_2[t_2_id] for t_1_id, t_2_id in matching.items()}
-        return matched_sent
+        action_matrix = self._get_action_matrix(cos_sim)
+        monotone_matching = self._get_monotone_matching(action_matrix)
+        matched_sent = {text_1[t_1_id]: text_2[t_2_id] for t_1_id, t_2_id in monotone_matching.items()}
+        return matched_sent if not need_matching_dict else (monotone_matching, matched_sent)
 
-    def _get_monotone_matchings(self, cos_sim) -> Dict[int, int]:
+    def _get_action_matrix(self, cos_sim):
         n, m = cos_sim.shape[0] + 1, cos_sim.shape[1] + 1
         if n < m:
             n, m = m, n
             cos_sim = cos_sim.T
         matrix = np.zeros((n, m))
-        for j in range(m):
-            matrix[j][j] = cos_sim[j - 1][j - 1].item()
+        actions_matrix = np.zeros((n, m))
+        for j in range(2, m):
+            actions_matrix[j][j] = self._next_group_flag
+        for j in range(1, m):
+            matrix[j][j] = cos_sim[j - 1][j - 1].item() + matrix[j - 1][j - 1]
+
         for i in range(1, n):  # text
             for j in range(1, min(i, m)):  # retell
                 sim = cos_sim[i - 1][j - 1].item()  # -1 в индексе из-за того, что матрица ответа обрамлена нулями
                 add_to_current_sentences_group = matrix[i - 1][j]
                 get_next_sentence_group = matrix[i - 1][j - 1]
                 best_choice = max(add_to_current_sentences_group, get_next_sentence_group)
+                if best_choice == add_to_current_sentences_group:
+                    actions_matrix[i][j] = self._current_group_flag
+                else:
+                    actions_matrix[i][j] = self._next_group_flag
                 matrix[i][j] = best_choice + sim
-        ans = self._recover_ans(matrix)
-        return ans
+        return actions_matrix
 
-    def _recover_ans(self, matrix) -> Dict[int, int]:
-        n, m = matrix.shape[0] - 1, matrix.shape[1] - 1,
+    def _get_monotone_matching(self, action_matrix):
+        n, m = action_matrix.shape[0] - 1, action_matrix.shape[1] - 1,
         ans = defaultdict()
         i, j = n, m
-        while i > 1 and j > 1:
-            add_to_current_sentences_group = matrix[i - 1, j]
-            get_next_sentence_group = matrix[i - 1, j - 1]
-            best_choice = min(add_to_current_sentences_group, get_next_sentence_group)
+        while i > 0 and j > 0:
+            action = action_matrix[i][j]
             ans[i - 1] = j - 1
-            if best_choice == add_to_current_sentences_group:
+            if action == self._current_group_flag:
                 i -= 1
-            else:
+            elif action == self._next_group_flag:
                 i -= 1
                 j -= 1
+            else:
+                break
 
         for q in range(1, i):
             ans[q - 1] = j - 1
