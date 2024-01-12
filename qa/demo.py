6441dfdee1bba64a7da7c29714143f19fd7a7e32
Dr1mzz <zaikinbogdan@mail.ru>
2024-01-13 01:27:13 +0500

    Add demo

--- qa/demo.py
+++
@@ -1,5 +0,0 @@
-from tg.common import DataBundle, Loc
-
-db = DataBundle.load(r'C:\Users\Nikolai\PycharmProjects\grammar_ru\demos\files\tsa-test.zip')
-
-print(Separator.Viewer().to_text(db.src.loc[db.src.sentence_id==1]))
